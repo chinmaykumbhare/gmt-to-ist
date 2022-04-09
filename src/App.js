53e2fddcd249d46038d5f108b25ab06f8f0103ac
Chinmay Kumbhare <thechinmaykumbhare@gmail.com>
2022-04-09 18:37:44 +0100

    bugfix: TZone adjust

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from "./logo.svg";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import "./App.css";
 
 function App() {
@@ -9,16 +9,21 @@ function App() {
   function getTime() {
     let timeString = "";
     const time = new Date();
-    timeString = (time.getHours() + 4) > 24 ? time.getHours() + 4 - 24 : time.getHours() + 4;
+    timeString = (time.getHours() + 5) > 24 ? time.getHours() + 5 - 24 : time.getHours() + 5;
     timeString += " : ";
-    timeString += (time.getMinutes() + 30) > 60 ? time.getMinutes() + 30 - 60 : time.getMinutes() + 30;
+    timeString += (time.getMinutes() + 30) > 60 ? "0" +(time.getMinutes() + 30 - 60) : time.getMinutes() + 30;
     // console.log(timeString);
     setTime(timeString);
   }
 
+  useEffect(() => {
+    console.log("init");
+    getTime();
+  }, [])
+
   setInterval((() => {
     getTime();
-  }), 1000);
+  }), 59000);
 
   return (
     <div className="App">
